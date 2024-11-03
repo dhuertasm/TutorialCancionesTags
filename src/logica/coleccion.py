f01faf6e54573a36574509baa2b769b855a8cb05
dhuertasm <dhuertasm@uniandes.edu.co>
2024-11-03 08:11:18 -0500

    prueba modificaciones

--- src/logica/coleccion.py
+++ src/logica/coleccion.py
@@ -214,3 +214,16 @@ class Coleccion():
         interpretes = [elem.__dict__ for elem in session.query(Interprete).filter(
             Interprete.nombre.ilike('%{0}%'.format(interprete_nombre))).all()]
         return interpretes
+    
+    def buscar_interprete(self, buscar, interprete_nombre):
+        buscar = True
+        if buscar:
+            interpretes = [elem.__dict__ for elem in session.query(Interprete).filter(
+            Interprete.nombre.ilike('%{0}%'.format(interprete_nombre))).all()]
+        return interpretes
+    
+    def dar_interpretes_2(self):
+        dar = True
+        if dar:
+            interpretes = [elem.__dict__ for elem in session.query(Interprete).all()]
+        return interpretes
